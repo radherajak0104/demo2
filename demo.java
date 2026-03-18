eb9b420964b32902c309e2b01a850fba23bdc615
radhe <radherajak0104@gmail.com>
2026-03-18 12:37:47 +0530

    first commit

--- demo.java
+++ demo.java
@@ -1,6 +1,14 @@
+// import java.util.*;
+import java.lang.Math;
+import java.util.Scanner;
+
 public class demo {
     public static void main(String[] args) {
-       System.out.println("Hello, World!");
-       System.err.println("volo tararara");
-    }
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Radius of the Circle:");
+    double r = sc.nextDouble();
+    double area = Math.PI * r * r;
+    System.out.println("The area of the circle is: " + area);
+    sc.close();
+ }
 }
